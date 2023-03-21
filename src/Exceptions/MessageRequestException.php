669e61e0ac0c877aac720021de1e9f2455c8b0eb
Missael H. Anda <31199350+MissaelAnda@users.noreply.github.com>
2023-03-21 11:38:15 -0600

    Better message error messages

--- src/Exceptions/MessageRequestException.php
+++ src/Exceptions/MessageRequestException.php
@@ -10,11 +10,11 @@ class MessageRequestException extends \Exception
 
     public function __construct(Response $response)
     {
-        $message = match ($response->status()) {
+        $message = $response->json('error.message', match ($response->status()) {
             404 => 'Resource not found',
             429 => 'Too many requests',
-            400 => $response->json('error.message', ''),
-        };
+            default => 'Error',
+        });
 
         parent::__construct($message, $response->json('error.code'));
         $this->error = $response->json('error', []);
